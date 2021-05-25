60f3ebc9357b4fa571a6a408b196b144c4e19ae2
Juan <jd.berbeo@gmail.com>
2021-05-24 19:02:12 -0500

    2do commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
+
+  const [count, setCount] = useState(0)
+
+    const handleOnChange = (e) => {
+     
+
+      setCount(e.target.value.length)
+     
+    }
+
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={handleOnChange}></textarea>
+        <div className="counter">{count}</div>
       </div>
     );
-  }
 }
 
 export default App;
